e70fb2fe1bec52023bce4bc42aadefc618b8b52a
Daniel Adesina <danieladesina999@hotmail.com>
2022-05-10 19:59:25 -0400

    added a comment to meal.js

--- backend/models/Meal.js
+++ backend/models/Meal.js
@@ -7,6 +7,7 @@ let Meal = new mongoose.Schema({
     recipe: {
         type: String
     }
+    //add a short description link and add an option in frontend where recipe links to actual recipe website or a note section w/ recipe
 });
 
 module.exports = mongoose.model('Meals', Meal);
