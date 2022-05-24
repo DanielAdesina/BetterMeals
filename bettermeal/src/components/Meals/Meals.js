70b5a4e89fb58c01d01134f0ad7fbfc429c0e87b
Daniel Adesina <danieladesina999@hotmail.com>
2022-05-24 16:48:48 -0400

    added search connected to spoonacular api

--- bettermeal/src/components/Meals/Meals.js
+++ bettermeal/src/components/Meals/Meals.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Meals.css"
+import axios from 'axios';
 
 
 class Meals extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            searchField: ""
+        }
+    }
+
+    handleClick(event){
+        event.preventDefault();
+        const options = {
+            method: 'GET',
+            url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search',
+            params: {query: this.state.searchField, number: '10', offset: '0'},
+            headers: {
+              'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+              'X-RapidAPI-Key': '51be17977amsh1e7b4aa1af671dep16977ajsn18b85345a72c'
+            }
+          };
+          axios.request(options).then(function (response) {
+              console.log(response.data);
+          }).catch(function (error) {
+              console.error(error);
+          });
+    }
+
     render() {
         return (
             <div class="container" id="meals-container">
@@ -11,7 +37,7 @@ class Meals extends Component {
                 
                     <div class="form-outline mb-4 input-group inner-addon right-addon">
                         
-                        <input type="text"  id="mealsearch" placeholder='Search for any meal!' />
+                        <input type="text"  id="mealsearch" placeholder='Search for any meal!' onChange={event => this.setState({searchField: event.target.value})}/>
                         
                         
                         {/* <span class="input-group-btn">
@@ -20,7 +46,7 @@ class Meals extends Component {
                         
                     </div>
                     <span class="input-group-btn">
-                        <button class="search-icon"></button>
+                        <button class="search-icon" type="submit" onClick={event => this.handleClick(event)}></button>
                     </span>
                 </form>
                 
