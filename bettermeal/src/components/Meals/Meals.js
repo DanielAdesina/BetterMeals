65bcd816f9ea8e7625a9f89d26a2fabc22d6a6a8
Daniel Adesina <danieladesina999@hotmail.com>
2022-05-25 23:14:31 -0400

    request caching for meal searches

--- bettermeal/src/components/Meals/Meals.js
+++ bettermeal/src/components/Meals/Meals.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Meals.css"
 import axios from 'axios';
+import Meal from './Meal';
 
 
 class Meals extends Component {
     constructor(props){
         super(props);
         this.state = {
-            searchField: ""
+            searchField: "",
+            mealsResult: <></>
         }
     }
 
@@ -22,12 +24,39 @@ class Meals extends Component {
               'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
               'X-RapidAPI-Key': '51be17977amsh1e7b4aa1af671dep16977ajsn18b85345a72c'
             }
-          };
-          axios.request(options).then(function (response) {
-              console.log(response.data);
-          }).catch(function (error) {
-              console.error(error);
-          });
+        };
+        const storageCheck = JSON.parse(localStorage.getItem(this.state.searchField));
+        // console.log(Date.now())
+        // console.log(storageCheck.ttl)
+        if(storageCheck && (Date.now() < storageCheck.ttl)){
+            alert("woohoo")
+            const mealArray = Array.from(storageCheck.results);
+            this.setState({
+                mealsResult: <div class="row row-cols-5">
+                    {mealArray.map((meal) =>
+                    <div class="col" style={{marginBottom: "2rem", height: "250px"}}><Meal id={meal.id}></Meal></div>)}
+                </div>
+            })
+        }
+        else{
+            
+            
+
+            axios.request(options).then(response => {
+                response.data.ttl = Date.now() + (86400 * 1000);
+                localStorage.setItem(this.state.searchField, JSON.stringify(response.data));
+                const mealArray = Array.from(response.data.results);
+                this.setState({
+                    mealsResult: <div class="row row-cols-5">
+                        {mealArray.map((meal) =>
+                        <div class="col" style={{marginBottom: "2rem", height: "250px"}}><Meal id={meal.id}></Meal></div>)}
+                    </div>
+                })
+            }).catch(function (error) {
+                console.error(error);
+            });
+        }
+          
     }
 
     render() {
@@ -49,6 +78,7 @@ class Meals extends Component {
                         <button class="search-icon" type="submit" onClick={event => this.handleClick(event)}></button>
                     </span>
                 </form>
+                {this.state.mealsResult}
                 
                                     
             </div>
